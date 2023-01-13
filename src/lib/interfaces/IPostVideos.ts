e8208c74d6ab70b3fce5ceedd76bbb1acdd3399f
Chufretalas <marcobene.contact@gmail.com>
2023-01-13 20:48:38 -0300

    users can now post a new video in the /videos endpoint

---
+++ src/lib/interfaces/IPostVideos.ts
@@ -0,0 +1,5 @@
+export default interface IPostVideo {
+    id?: number,
+    titulo: string,
+    descricao: string
+}
--- src/routes/videos/+server.ts
+++ src/routes/videos/+server.ts
@@ -1,11 +1,58 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, type aluraflix_videos } from '@prisma/client';
+import type IPostVideo from '$lib/interfaces/IPostVideos';
+
+async function createEntry(titulo: string, descricao: string): Promise<aluraflix_videos> {
+    const dbCreateResponse = await prisma.aluraflix_videos.create({
+        data: {
+            titulo: titulo,
+            descricao: descricao,
+            url: "http://localhost:5173/videos/"
+        }
+    })
+    const dbUpdateResponse = await prisma.aluraflix_videos.update({
+        where: {
+            id: dbCreateResponse.id
+        },
+        data: {
+            url: dbCreateResponse.url + dbCreateResponse.id
+        }
+    })
+    return dbUpdateResponse
+}
 
 const prisma = new PrismaClient()
+
 
 export const GET = (async ({ url }) => {
     let allVideos = await prisma.aluraflix_videos.findMany()
     prisma.$disconnect()
     return new Response(JSON.stringify(allVideos))
 }) satisfies RequestHandler;
+
+
+export const POST = (async ({ request }) => {
+    try {
+        const requestData: IPostVideo = await request.json()
+
+        if (requestData.titulo !== undefined && requestData.descricao !== undefined) {
+
+            if (!requestData.titulo) throw new Error("titulo não pode ser vazio")
+            else if (requestData.titulo.length > 40) throw new Error("titulo não pode ter mais que 40 caracteres")
+            else if (!requestData.descricao) throw new Error("descrição não pode ser vazia")
+
+            const dbResponse = await createEntry(requestData.titulo, requestData.descricao)
+            prisma.$disconnect()
+
+            return new Response(JSON.stringify(dbResponse))
+        }
+
+        throw new Error("incomplete body input")
+
+    } catch (err: any) {
+        throw error(400, "Erro no POST: " + err.message)
+    }
+}) satisfies RequestHandler
+
+prisma.$disconnect()
