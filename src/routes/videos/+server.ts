96a69c08cf46be3fa19e8ca897b0208d9d4a7f39
Chufretalas <marcobene.contact@gmail.com>
2023-01-16 11:39:41 -0300

    you can now specify the id when posting a new video and the main GET is now ordered

--- src/routes/videos/+server.ts
+++ src/routes/videos/+server.ts
@@ -3,7 +3,26 @@ import type { RequestHandler } from './$types';
 import { PrismaClient, type aluraflix_videos } from '@prisma/client';
 import getJson from '$lib/server/api_helpers/getJson';
 
-async function createEntry(titulo: string, descricao: string): Promise<aluraflix_videos> {
+async function createEntry(id: number | null, titulo: string, descricao: string): Promise<aluraflix_videos> {
+    if (id) {
+        try {
+            const dbResponse = await prisma.aluraflix_videos.create({
+                data: {
+                    id: id,
+                    titulo: titulo,
+                    descricao: descricao,
+                    url: `http://localhost:5173/videos/${id}`
+                }
+            })
+            return dbResponse
+        } catch (err: any) {
+            if (err.message.includes("Unique constraint failed on the fields: (`id`)")) {
+                throw error(400, `video com id ${id}, já existe`)
+            }
+            throw error(500, "unkown error")
+        }
+    }
+
     const dbCreateResponse = await prisma.aluraflix_videos.create({
         data: {
             titulo: titulo,
@@ -27,7 +46,9 @@ const prisma = new PrismaClient()
 
 export const GET = (async ({ url }) => {
     try {
-        let allVideos = await prisma.aluraflix_videos.findMany()
+        let allVideos = await prisma.aluraflix_videos.findMany({
+            orderBy: [{ id: "asc" }]
+        })
         prisma.$disconnect()
         return new Response(JSON.stringify(allVideos))
     } catch (er) {
@@ -44,19 +65,22 @@ export const POST = (async ({ request }) => {
 
         if (!success) throw error(400, "body inválido")
 
+        if (data.id) {
+            if (isNaN(+data.id) || !(Number.isInteger(+data.id))) throw error(400, "valor opcional 'id' deve ser um número inteiro")
+        }
         if (!("titulo" in data && "descricao" in data)) throw error(400, "campos 'titulo e descricao são obrigatórios")
         if (!data.titulo || data.titulo.length > 40) throw error(400, "titulo não pode ser vazio e tem limite de 40 caracteres")
         if (!data.descricao) throw error(400, "descrição não pode ser vazia")
 
-        const dbResponse = await createEntry(data.titulo, data.descricao)
+        const dbResponse = await createEntry(data.id ?? null, data.titulo, data.descricao)
         prisma.$disconnect()
 
         return new Response(JSON.stringify({ newVideo: dbResponse }))
 
-    } 
+    }
     catch (err: any) {
         prisma.$disconnect()
-        if(err.status == 400) throw err
+        if (err.status == 400) throw err
         throw error(500, "unkown error")
     }
 }) satisfies RequestHandler
