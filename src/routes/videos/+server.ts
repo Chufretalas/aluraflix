8f473a39a37fcdb5bd1e82848a2eabae4d4a6e7c
Chufretalas <marcobene.contact@gmail.com>
2023-01-18 16:03:17 -0300

    fixed a problem that happened when manually POSTing a video id that is higher than the autoincrement

--- src/routes/videos/+server.ts
+++ src/routes/videos/+server.ts
@@ -17,7 +17,14 @@ async function createEntry(id: number | undefined, titulo: string, descricao: st
         return dbResponse
     } catch (err: any) {
         if (err.message.includes("Unique constraint failed on the fields: (`id`)")) {
-            throw error(400, `video com id ${id}, já existe`)
+            if (id === undefined) { // fixes a problem when an id that is higher than the autoincremenet is manually put in
+                try {
+                    return await createEntry(id, titulo, descricao, categoria_id)
+                } catch (err2) {
+                    throw error(500, "unkown error")
+                }
+            }
+            throw error(400, `video com id: ${id}, já existe`)
         }
         if (err.message.includes("Foreign key constraint failed on the field: `aluraflix_videos_categoria_id_fkey (index)`")) {
             throw error(400, `categoria_id: ${categoria_id} não é válido`)
@@ -58,7 +65,7 @@ export const POST = (async ({ request }) => {
         if (!data.descricao) throw error(400, "descrição não pode ser vazia")
         if (data.categoria_id && (isNaN(+data.categoria_id) || !Number.isInteger(+data.categoria_id))) throw error(400, "categoria_id deve ser um numero inteiro")
 
-        const dbResponse = await createEntry(data.id, data.titulo, data.descricao, +data.categoria_id)
+        const dbResponse = await createEntry(data.id, data.titulo, data.descricao, data.categoria_id)
         prisma.$disconnect()
 
         return new Response(JSON.stringify({ newVideo: dbResponse }))
